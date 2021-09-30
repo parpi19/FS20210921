53764c54f1db9688a44b173fb28de2753295c361
parpi19 <parpi19@gmail.com>
2021-09-30 16:32:25 +0200

    correccion ejercicioshtml

--- fundamentos/Sakila/js/ejercicios-js.js
+++ fundamentos/Sakila/js/ejercicios-js.js
@@ -1,69 +1,155 @@
-function numAleatorio (a, b){
-    return parseInt(Math.random() * (b - a) + a);
+function numAleatorio(minimo, maximo) {
+  return parseInt(Math.random() * (maximo - minimo) + minimo);
 }
 
-console.log("El numeor aleatorio del Ejercicio 1 es: "+ numAleatorio(0,100));
+console.log("El numeor aleatorio del Ejercicio 1 es: " + numAleatorio(0, 100));
 
-function compara(a,b){
-    if(a==b) {
-        return "son iguales"
-    }else if(a < b){
-        return "es mayor"
-    }else {
-        return "es menor"
-    }
-
-function isNum(a) {
-    
-}
-
-}
-function adivinar(){
-    let numeroAleatorio = numAleatorio(1,5);
-    let contador = 5;
-    let numeroIntroducido;
-    do {
-        numeroIntroducido = prompt(
-          `Ingrese un número (quedan ${contador} intentos):`
-        );
-        let a = compara(numeroIntroducido, numeroAleatorio);
-        if (a == "es menor"){
-            alert("El numero introducido es mayor ");
-            
-        }else if (a == "es mayor"){
-            alert("El numero introducido es menor");
-            
-        }else {
-            alert("Has acertado el número");
-            
-        }
-        contador --;
-      } while(numeroAleatorio != numeroIntroducido && contador > 0);
-
-}
- // console.log(adivinar());
-
-function esNum(b) {
-    return isNaN(b);
-    
-}
-
-
-function devolverArray(a) {
-    let array = [];
-    if(isNaN(a)=== false){
-        for(let contador = 0; contador < a; contador++){
-            array.push(` ${contador+1}`);
-        }
-          alert(array);
-    }else 
-    alert("No es un numero");
-    
+function compara(numeroA, numeroB) {
+  if (numeroA == numeroB) {
+    return "son iguales";
+  } else if (numeroA < numeroB) {
+    return "es mayor";
+  } else {
+    return "es menor";
   }
-console.log(devolverArray("7"));
+}
+function adivinar() {
+  let numeroAleatorio = numAleatorio(1, 5);
+  let contador = 5;
+  let numeroIntroducido;
+  do {
+    numeroIntroducido = prompt(
+      `Ingrese un número (quedan ${contador} intentos):`
+    );
+    let a = compara(numeroIntroducido, numeroAleatorio);
+    if (a == "es menor") {
+      alert("El numero introducido es mayor ");
+    } else if (a == "es mayor") {
+      alert("El numero introducido es menor");
+    } else {
+      alert("Has acertado el número");
+    }
+    contador--;
+  } while (numeroAleatorio != numeroIntroducido && contador > 0);
+}
+//console.log(adivinar());
 
+function devolverArray(longitud, valor) {
+  let array = [];
+  if (isNaN(longitud) === false) {
+    for (let contador = 0; contador < longitud; contador++) {
+      array.push(valor);
+    }
+    return array;
+  }
+}
+console.log(devolverArray("7", "al"));
+
+function esPrimo(numero) {
+  if (numero == 0 || numero == 1) return false;
+  for (let i = 2; i < numero; i++) {
+    if (numero % i == 0) return false;
+  }
+  return true;
+}
+
+function numerosPrimos(a, b) {
+  let arrayPrimos = [];
+  for (let i = a; i < b; i++) {
+    if (esPrimo(i)) {
+      arrayPrimos.push(i);
+    }
+  }
+  return arrayPrimos;
+}
+
+console.log(numerosPrimos(0, 50));
 
 function validaPalindromo(caso) {
-    return true;
-    
+  return true;
+}
+
+
+
+function Juego(maxIntentos, valores) {
+  var numeroBuscado;
+  this.intentos;
+  this.encontrado;
+  this.mensaje;
+  this.Inicializa = function () {
+      numeroBuscado = aleatorio(1, valores);
+      this.intentos = 0;
+      this.encontrado = false;
+      this.mensaje = 'Listo para jugar';
+  }
+
+  this.Inicializa();
+
+  this.PruebaCon = function (numeroIntroducido) {
+      if (this.intentos >= maxIntentos)
+          throw new Error("Excedido el numero de intentos");
+      this.intentos += 1;
+      if (numeroBuscado == numeroIntroducido) {
+          this.encontrado = true;
+          this.mensaje = 'Bieeen!!! Acertaste.';
+          return this.mensaje;
+      }
+      if (this.intentos >= maxIntentos) {
+          this.mensaje = 'Upsss! Se acabaron los intentos, el número era el ' + numeroBuscado;
+          return this.mensaje;
+      }
+      if (numeroBuscado > numeroIntroducido) {
+          this.mensaje = 'Mi número es mayor.';
+          return this.mensaje;
+      }
+      this.mensaje = 'Mi número es menor.';
+      return this.mensaje;
+  }
+  this.DameMaxIntentos = function () { return maxIntentos; }
+}
+
+//Juego.prototype.DameMaxIntentos = function() { return maxIntentos; }
+Juego.prototype.DameIntento = function () { return this.intentos + 1; }
+
+class JuegoConClase {
+  #maxIntentos;
+  #valores;
+  #numeroBuscado;
+  constructor(maxIntentos, valores) {
+      this.#maxIntentos = maxIntentos;
+      this.#valores = valores;
+      this.Inicializa();
+  }
+  Inicializa() {
+      this.#numeroBuscado = aleatorio(1, this.#valores);
+      this.intentos = 0;
+      this.encontrado = false;
+      this.mensaje = 'Listo para jugar';
+  };
+  PruebaCon(numeroIntroducido) {
+      if (this.intentos >= this.#maxIntentos)
+          throw new Error("Excedido el numero de intentos");
+      this.intentos += 1;
+      if (this.#numeroBuscado == numeroIntroducido) {
+          this.encontrado = true;
+          this.mensaje = 'Bieeen!!! Acertaste.';
+          return this.mensaje;
+      }
+      if (this.intentos >= this.#maxIntentos) {
+          this.mensaje = 'Upsss! Se acabaron los intentos, el número era el ' + this.#numeroBuscado;
+          return this.mensaje;
+      }
+      if (this.#numeroBuscado > numeroIntroducido) {
+          this.mensaje = 'Mi número es mayor.';
+          return this.mensaje;
+      }
+      this.mensaje = 'Mi número es menor.';
+      return this.mensaje;
+  };
+
+  DameMaxIntentos() { return this.#maxIntentos; }
+
+  get maxIntentos() { return this.#maxIntentos; };
+
+  get intento() { return this.intentos + 1; }
 }
--- fundamentos/spec/ejercicios.spec.js
+++ fundamentos/spec/ejercicios.spec.js
@@ -10,27 +10,103 @@ describe('Ejercicio 1', function(){
 
         expect(resultado).toBeLessThan(b);
         expect(resultado).toBeGreaterThanOrEqual(a);
+    });
+
+    it('No son numeros', function(){
+        let a = undefined, b = "hola";
+        let resultado;
+
+        
+        resultado = numAleatorio(a,b);
+
+        expect(resultado).toBeNaN();
+        
     })
 })
 
+
+describe('Ejercicio 2: Adivina el Número', () => {
+    let juego = null;
+    const NUM_INTENTOS = 10;
+
+    beforeAll(() => {
+        spyOn(Math, 'random').and.returnValue(0.83435)
+    });
+
+    beforeEach(() => {
+        juego = new JuegoConClase(NUM_INTENTOS, 100);
+    });
+
+    it('Mi número es mayor.', () => {
+        juego.PruebaCon(82)
+        expect(juego.mensaje).toBe('Mi número es mayor.')
+        expect(juego.intentos).toBe(1)
+        expect(juego.encontrado).toBeFalse()
+    });
+
+    it('Mi número es menor.', () => {
+        juego.PruebaCon(84)
+        juego.PruebaCon(84)
+        expect(juego.mensaje).toBe('Mi número es menor.')
+        expect(juego.intentos).toBe(2)
+        expect(juego.encontrado).toBeFalse()
+    });
+
+    it('Intentos', () => {
+        for(let i = 1; i < NUM_INTENTOS; i++) {
+            juego.PruebaCon(1)
+            expect(juego.intentos).toBe(i)
+        }
+        juego.PruebaCon(1)
+        expect(juego.mensaje).toBe('Upsss! Se acabaron los intentos, el número era el 83')
+        expect(juego.intentos).toBe(NUM_INTENTOS)
+        expect(juego.encontrado).toBeFalse()
+    });
+
+    it('Bieeen!!! Acertaste.', () => {
+        juego.PruebaCon(83)
+        expect(juego.mensaje).toBe('Bieeen!!! Acertaste.')
+        expect(juego.encontrado).toBeTrue()
+    });
+
+    it('Excedido el numero de intentos', () => {
+        for(let i = 0; i < NUM_INTENTOS; i++) {
+            juego.PruebaCon(1)
+        }
+        expect(() => juego.PruebaCon(1)).toThrow()
+        expect(juego.intentos).toBe(NUM_INTENTOS)
+        expect(juego.encontrado).toBeFalse()
+    });
+
+    xit('No es un número', () => {
+        juego.PruebaCon('otra cosa')
+        expect(juego.intentos).toBe(0)
+        expect(juego.encontrado).toBeFalse()
+    });
+
+})
 
 describe('Ejercicio 3'), () => {
     it('Que sea numero', () => {
         let num = 2;
         let num2 = "2";
 
-        expect(esNum(num)).toBeFalse;
-        expect(esNum(num2)).toBeFalse;   
+        expect(isNaN(num)).toBeFalse();
+        expect(isNaN(num2)).toBeFalse();   
     })
     
     it('Que no sea numero', () => {
         let num = undefined;
         let num2 = "no es numero";
 
-        expect(esNum(num)).toBeTrue;
-        expect(esNum(num2)).toBeTrue;   
+        expect(isNaN(num)).toBeTrue();
+        expect(isNaN(num2)).toBeTrue();   
     })
 }
+
+/*describe('Ejercicio 4', () => {
+    it('E')
+})
 
 /*describe('Ejercicio 6', function (){
     ['ana','oso', 'Sometamos o matemos', 'Isaac no ronca así'].forEach(caso => {
