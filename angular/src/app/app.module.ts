3d5c626710f48ef6d12d1170246871ba9e48e855
parpi19 <parpi19@gmail.com>
2021-10-05 16:17:17 +0200

    pipe y sizer

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,13 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DemosComponent } from './demos/demos.component';
+import { FormsModule } from '@angular/forms';
+import { ElipsisPipe } from './compartidos/cadenas.pipe';
+import { SizerComponent } from './compartidos/sizer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DemosComponent,
+    ElipsisPipe,
+    SizerComponent,
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, FormsModule,
     AppRoutingModule
   ],
   providers: [],
