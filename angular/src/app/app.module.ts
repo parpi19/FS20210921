420177651e93127fb58696125e91ff73d55590a1
parpi19 <parpi19@gmail.com>
2021-10-06 13:55:25 +0200

    cambios estructuracion

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,27 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { DemosComponent } from './demos/demos.component';
 import { FormsModule } from '@angular/forms';
-import { ElipsisPipe } from './compartidos/cadenas.pipe';
-import { SizerComponent } from './compartidos/sizer.component';
 import { DinamicoComponent } from './dinamico/dinamico.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { MyCoreModule } from 'src/lib/my-core';
+import { MainModule } from './main';
+import { CommonModule } from '@angular/common';
+import { SecurityModule } from './security';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     DemosComponent,
-    ElipsisPipe,
-    SizerComponent,
     DinamicoComponent,
     CalculadoraComponent,
   ],
   imports: [
     BrowserModule, FormsModule,
-    AppRoutingModule
+    AppRoutingModule, MyCoreModule, MainModule, CommonModule, SecurityModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular/src/lib/my-core/pipes/cadenas.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'elipsis'
+})
+export class ElipsisPipe implements PipeTransform{
+  transform(value: any, maxlen: number): any {
+    return (!maxlen || maxlen < 2 || !value || value.length <= maxlen) ? value : (value.substr(0, maxlen - 1) + '\u2026');;
+  }
+}
+
+@Pipe({
+  name: 'toComaDecimal'
+})
+export class ToComaDecimalPipe implements PipeTransform {
+  transform(value: any, args?: any): any {
+    if (typeof (value) === 'number') {
+      value = value.toString();
+    }
+    if (typeof (value) === 'string') {
+      return value.replace(/\./g, ',');
+    }
+    return value;
+  }
+}
+
+export const PIPES_CADENAS = [ ElipsisPipe, ToComaDecimalPipe ];
