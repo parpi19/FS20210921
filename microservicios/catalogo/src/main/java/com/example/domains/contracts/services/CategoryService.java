e04a20090518c91f26cbd947f87b8ee8d9f659cb
parpi19 <parpi19@gmail.com>
2021-10-22 11:36:15 +0200

    catalogo

---
+++ microservicios/catalogo/src/main/java/com/example/domains/contracts/services/CategoryService.java
@@ -0,0 +1,10 @@
+package com.example.domains.contracts.services;
+
+import com.example.domains.core.services.contracts.DomainService;
+import com.example.domains.entities.Category;
+
+public interface CategoryService extends DomainService<Category, Integer>{
+
+
+
+}
